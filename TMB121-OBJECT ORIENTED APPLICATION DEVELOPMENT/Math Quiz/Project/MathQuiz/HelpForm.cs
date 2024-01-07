c36a969bbffd34d9d934350877653f41c0b0f53c
DrDruid <acharitoudis@gmail.com>
2024-01-07 15:01:52 +0200

    A semester group project for TMB121-OBJECT ORIENTED APPLICATION DEVELOPMENT in C#.
    The final project for the course TMB121-OBJECT ORIENTED APPLICATION DEVELOPMENT. Where every person in the group had to make a different application. My application was a quiz game, with a calculator in it to help if someone is struggling., timer, and points system ( for each correct answer the participant will be awarded 100 points ) .

---
+++ TMB121-OBJECT ORIENTED APPLICATION DEVELOPMENT/Math Quiz/Project/MathQuiz/HelpForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MathQuiz
+{
+    public partial class HelpForm : Form
+    {
+        public HelpForm()
+        {
+            InitializeComponent();
+        }
+
+        private void HelpForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                Hide();
+            }
+        }
+    }
+}
